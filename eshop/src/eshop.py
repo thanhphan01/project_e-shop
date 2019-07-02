819933a26c3623a4a4748654e9c44d6f52d52760
Jonas Houda <jonas.houda@students.gymneufeld.ch>
2019-07-02 16:15:08 +0200

    Login works, css stylesheet works, right homepage html version but homepage css buggy

--- eshop/src/eshop.py
+++ eshop/src/eshop.py
@@ -3,14 +3,19 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
-# MySQL configurations
-#app.config[’MYSQL_USER’] = ’root’
-#app.config[’MYSQL_DB’] = Database_project_e_shop
-#app.config[’MYSQL_HOST’] = ’localhost’
-#mysql = MySQL(app)
+#MySQL configurations
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_DB'] = 'project_e_shop'
+app.config['MYSQL_HOST'] = 'localhost'
+mysql = MySQL(app)
 
-def check_password(key,password):
-    return key==password
+def check_password(username,password):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT pword FROM user where uname = '%s'" %request.form['username'])
+    rv = cursor.fetchall()
+    if len(rv) == 1:
+        if rv[0][0] == request.form['password']:
+            return True
 
 @app.route('/', methods=['GET', 'POST'])         #Login/Home
 def login():
@@ -18,9 +23,7 @@ def login():
     if request.method == 'POST':
         session['uname'] = request.form['username']
         
-        #key = 
-       
-        if check_password(key, request.form['password']):
+        if check_password(request.form['username'],request.form['password']):
             session['logged_in'] = True
             return redirect(url_for('loggedIn'))
     return render_template('homepage.html', message=message)
@@ -29,7 +32,7 @@ def login():
 def loggedIn():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    return render_template('LoggedIn.html', firstname=session['fname'], lastname=session['lname'])
+    return render_template('logged_in.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
