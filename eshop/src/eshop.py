bc4abe42300390bcbd3edd479692601de3100724
Jonas Houda <jonas.houda@students.gymneufeld.ch>
2019-07-02 14:09:10 +0200

    Login works with static password

---
+++ eshop/src/eshop.py
@@ -0,0 +1,35 @@
+from flask import *
+#from flask.ext.mysqldb import MySQL
+app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+# MySQL configurations
+#app.config[’MYSQL_USER’] = ’root’
+#app.config[’MYSQL_DB’] = ########################
+#app.config[’MYSQL_HOST’] = ’localhost’
+#mysql = MySQL(app)
+
+def check_password(key,password):
+    return key==password
+
+@app.route('/', methods=['GET', 'POST'])         #Login/Home
+def login():
+    message = ''
+    if request.method == 'POST':
+        session['fname'] = request.form['firstname']
+        session['lname'] = request.form['lastname']
+       
+        key = '1234'
+        if check_password(key, request.form['password']):
+            session['logged_in'] = True
+            return redirect(url_for('loggedIn'))
+    return render_template('homepage.html', message=message)
+
+@app.route('/loggedIn', methods=['GET', 'POST'])
+def loggedIn():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('LoggedIn.html', firstname=session['fname'], lastname=session['lname'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
