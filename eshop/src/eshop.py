73949b6983f9cf8d6d34193f0aa31140c0aa26a8
Jonas Houda <jonas.houda@students.gymneufeld.ch>
2019-07-03 11:32:54 +0200

    Links between homepage shopping cart and logged in, item list implemented for homepage and logged in picture not working yet

--- eshop/src/eshop.py
+++ eshop/src/eshop.py
@@ -1,5 +1,6 @@
 from flask import *
 from flask_mysqldb import MySQL
+from base64 import *
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
@@ -9,15 +10,21 @@ app.config['MYSQL_DB'] = 'project_e_shop'
 app.config['MYSQL_HOST'] = 'localhost'
 mysql = MySQL(app)
 
-def check_password(username,password):
+def check_password(username,password):                                          #checks password
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT pword FROM user where uname = '%s'" %request.form['username'])
     rv = cursor.fetchall()
     if len(rv) == 1:
         if rv[0][0] == request.form['password']:
             return True
-
-@app.route('/', methods=['GET', 'POST'])         #Login/Home
+        
+def product_list():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT picture, name, price, stock FROM products")
+    rv = cursor.fetchall()
+    #rv[0] = b64encode(rv[0])
+    return rv
+@app.route('/', methods=['GET', 'POST'])                                        #Login/Home
 def login():
     message = ''
     if request.method == 'POST':
@@ -26,13 +33,17 @@ def login():
         if check_password(request.form['username'],request.form['password']):
             session['logged_in'] = True
             return redirect(url_for('loggedIn'))
-    return render_template('homepage.html', message=message)
+    return render_template('homepage.html', message=message, rv = product_list())
 
 @app.route('/loggedIn', methods=['GET', 'POST'])
 def loggedIn():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    return render_template('logged_in.html')
+    return render_template('logged_in.html', rv = product_list())
+
+@app.route('/shopping_cart', methods=['GET', 'POST'])
+def shopping_cart():
+    return render_template('shopping_cart.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
